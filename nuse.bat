48e72cf63171bfea0a058c127da4b8d924bd6a89
Valentin Ianchis <vallyian@gmail.com>
2022-05-16 18:46:02 +0300

    Test workflow (#1)
    Test workflow

--- nuse.bat
+++ nuse.bat
@@ -1,45 +1,58 @@
 @echo off
 
+set debugger=%2
+if "%debugger%"=="true" echo [34m[debug] debugger = '%debugger%' [0m
+
 ::env
+if "%nuseDir%"=="" (
+    if "%debugger%"=="true" echo [34m[debug] nuseDir == '' [0m
+    set nuseDir "%userprofile%\.nuse"
+    setx nuseDir "%userprofile%\.nuse"
+    set "PATH=%userprofile%\.nuse;%path%"
+)
 set "nodeDistUrl=https://nodejs.org/dist"
-set "nuseDirFile=%userprofile%\.nuse\v"
+set "nuseDirFile=%nuseDir%\v"
 
 ::install nuse
-if not exist "%userprofile%\.nuse" (
-    mkdir "%userprofile%\.nuse"
+if not exist "%nuseDir%" (
+    if "%debugger%"=="true" echo [34m[debug] nuseDir '%nuseDir%' not exist [0m
+    mkdir "%nuseDir%"
     set "nuseInitRegistry=1"
 )
 pushd "%~dp0"
 set "currentPath=%~dp0%~nx0"
-if not exist "%userprofile%\.nuse\nuse.bat" (
-    copy "%~dp0%~nx0" "%userprofile%\.nuse\nuse.bat" >nul
+if "%debugger%"=="true" echo [34m[debug] currentPath = '%currentPath%' [0m
+if not exist "%nuseDir%\nuse.bat" (
+    if "%debugger%"=="true" echo [34m[debug] nuse.bat '%nuseDir%\nuse.bat' not exist [0m
+    copy "%~dp0%~nx0" "%nuseDir%\nuse.bat" >nul
     set "nuseInitRegistry=1"
 )
 popd
-if not exist "%userprofile%\.nuse\nuse.ps1" (
-    echo nuse.bat @args > "%userprofile%\.nuse\nuse.ps1"
+if not exist "%nuseDir%\nuse.ps1" (
+    if "%debugger%"=="true" echo [34m[debug] nuse.ps1 '%nuseDir%\nuse.ps1' not exist [0m
+    echo nuse.bat @args > "%nuseDir%\nuse.ps1"
     set "nuseInitRegistry=1"
 )
-if not exist "%userprofile%\.nuse\node-latest.exe" (
+if not exist "%nuseDir%\node-latest.exe" (
     echo getting latest node executable %nodeDistUrl%/latest/win-x64/node.exe ...
-    curl "%nodeDistUrl%/latest/win-x64/node.exe" -o "%userprofile%\.nuse\node-latest.exe" >nul
+    if "%debugger%"=="true" echo [34m[debug] curl  '%nodeDistUrl%/latest/win-x64/node.exe' -o '%nuseDir%\node-latest.exe' [0m
+    curl "%nodeDistUrl%/latest/win-x64/node.exe" -o "%nuseDir%\node-latest.exe" >nul
     set "nuseInitRegistry=1"
 )
-if not exist "%userprofile%\.nuse\n-use.js" (
+if not exist "%nuseDir%\n-use.js" (
     echo getting nuse runner https://raw.githubusercontent.com/vallyian/nuse/main/n-use.js ...
-    curl https://raw.githubusercontent.com/vallyian/nuse/main/n-use.js -o "%userprofile%\.nuse\n-use.js" >nul
+    if "%debugger%"=="true" echo [34m[debug] curl https://raw.githubusercontent.com/vallyian/nuse/main/n-use.js -o '%nuseDir%\n-use.js' [0m
+    curl https://raw.githubusercontent.com/vallyian/nuse/main/n-use.js -o "%nuseDir%\n-use.js" >nul
     set "nuseInitRegistry=1"
-)
-if "%nuseDir%"=="" (
-    setx nuseDir "%userprofile%\.nuse"
-    set "PATH=%userprofile%\.nuse;%path%"
 )
 
 ::get wanted node version
-"%userprofile%\.nuse\node-latest.exe" "%userprofile%\.nuse\n-use.js" "%1" || exit /b 1
+if "%debugger%"=="true" echo [34m[debug] call '%nuseDir%\node-latest.exe' '%nuseDir%\n-use.js' '%1' [0m
+"%nuseDir%\node-latest.exe" "%nuseDir%\n-use.js" "%1" || exit /b 1
 
 ::set new node path
 set /p nodeDir=<"%nuseDirFile%"
+if "%debugger%"=="true" echo [34m[debug] nodeDir = '%nodeDir%' [0m
 setx nodeDir "%nodeDir%" >nul
 set "PATH=%nodeDir%;%path%"
 
@@ -51,7 +64,11 @@ echo [33mnode: %nodever%[0m
 echo [33m npm: v%npmver%[0m
 
 ::cleanup
-if not "%currentPath%"=="%userprofile%\.nuse\nuse.bat" (
+if not "%currentPath%"=="%nuseDir%\nuse.bat" (
+if not "%nuseDir%"=="." (
+    if "%debugger%"=="true" echo [34m[debug] currentPath '%currentPath%' != '%nuseDir%\nuse.bat' [0m
+    if "%debugger%"=="true" echo [34m[debug] del '%currentPath%' [0m
 	del "%currentPath%"
 	exit
+)
 )
--- test.js
+++ test.js
@@ -6,33 +6,47 @@ describe('nuse', () => {
     let nuseDir = '';
     let nodeDir = '';
 
-    it('args', () => !nodev && !npmv && assert.fail(`invalid test run args`));
-
-    it('user env Path is updated', async () => {
-        const userPath = await execCmd('reg', 'query', 'HKCU\\Environment', '/v', 'Path')
-            .then(x => (x.trim().split(/\s{4}/g)[3] || '').split(";"));
-        assert.equal(userPath.includes('%nuseDir%'), true, `user env Path does not include %nuseDir%`);
-        assert.equal(userPath.includes('%nodeDir%'), true, `user env Path does not include %nodeDir%`);
-    });
+    if (!nodev && !npmv)
+        assert.fail(`invalid test run args`);
 
     it('user env nuseDir is set', async () => {
-        nuseDir = await execCmd('reg', 'query', 'HKCU\\Environment', '/v', 'nuseDir')
-            .then(x => (x.trim().split(/\s{4}/g)[3] || ''));
-        assert.notEqual(nuseDir, '');
+        nuseDir = process.env.NUSE_GHA_RUNNER === 'true'
+            ? process.env.nuseDir
+            : await execCmd('reg', 'query', 'HKCU\\Environment', '/v', 'nuseDir')
+                .then(x => (x.trim().split(/\s{4}/g)[3] || ''));
+        expect(nuseDir).not.toEqual('');
     });
 
     it('user env nodeDir is set', async () => {
         nodeDir = await execCmd('reg', 'query', 'HKCU\\Environment', '/v', 'nodeDir')
             .then(x => (x.trim().split(/\s{4}/g)[3] || ''));
-        assert.notEqual(nodeDir, '');
+        expect(nodeDir).not.toEqual('');
     });
 
-    it('node and npm verions', async () => {
-        const out = await execCmd('nuse.bat', '-v');
-        if (nodev) assert.equal(out.includes(`node: v${nodev}`), true, `expected node version: '${nodev}'; out: '${out}'`);
-        if (npmv) assert.equal(out.includes(`npm: v${npmv}`), true, `expected npm version: '${npmv}'; out: '${out}'`);
+    it('user env Path includes %nuseDir%', async () => {
+        const userPath = await execCmd('reg', 'query', 'HKCU\\Environment', '/v', 'Path')
+            .then(x => (x.trim().split(/\s{4}/g)[3] || '').split(";"));
+        expect(userPath).toInclude('%nuseDir%');
+    });
+
+    it('user env Path includes %nodeDir%', async () => {
+        const userPath = await execCmd('reg', 'query', 'HKCU\\Environment', '/v', 'Path')
+            .then(x => (x.trim().split(/\s{4}/g)[3] || '').split(";"));
+        expect(userPath).toInclude('%nodeDir%');
+    });
+
+    it('node verion', async () => {
+        if (!nodev) return 'skip';
+        expect(await execCmd(`${nodeDir}\\node.exe`, '-v')).toMatch(`^v${nodev}`);
+    });
+
+    it('npm verion', async () => {
+        if (!npmv) return 'skip';
+        expect(await execCmd(`${nodeDir}\\npm.cmd`, '-v')).toMatch(`^${npmv}`);
     });
 });
+
+// helpers
 
 function execCmd(cmd, ...args) {
     return new Promise((ok, rej) => child_process.execFile(
@@ -43,12 +57,46 @@ function execCmd(cmd, ...args) {
     ));
 }
 
+// test fw
+
+process.on('exit', () => global.tests.failed && process.exit(1));
+
 async function describe(name, cb) {
     if (!global.tests) global.tests = [];
+    console.log(`\n\x1b[36m${name}\x1b[0m`);
     await Promise.resolve().then(cb);
-    console.log(`\x1b[36m${name}\x1b[0m`);
-    for (let test of global.tests)
-        console.log(`\x1b[34m    ${test.name}\x1b[0m ... ${await Promise.resolve().then(test.cb).then(() => '\x1b[32mpass\x1b[0m').catch(() => '\x1b[31mfail\x1b[0m')}`);
+    for (let test of global.tests) {
+        process.stdout.write(`    \x1b[34m${test.name}\x1b[0m ... `);
+        process.stdout.write(await Promise.resolve().then(test.cb)
+            .then(r => r === 'skip' ? 'skipped\n' : '\x1b[32mpass\x1b[0m\n')
+            .catch(err => {
+                global.tests.failed = true;
+                return `\x1b[31mfail\n        ${err.message || err}\x1b[0m\n`;
+            }));
+    }
+    process.stdout.write('\n');
 }
 
 function it(name, cb) { global.tests.push({ name, cb }); }
+
+function expect(actual) {
+    let notFlag = false;
+    const run = (matcher, expected, func) => {
+        const ret = func(expected);
+        if ((ret === true && notFlag === true) ||
+            (ret === false && notFlag === false))
+            assert.fail(`expect: '${actual}' ${matcher}: '${expected}'`);
+    }
+    const matchers = {
+        toInclude: expected => run('toInclude', expected, expected => actual.includes(expected)),
+        toEqual: expected => run('toEqual', expected, expected => actual === expected),
+        toMatch: expected => run('toMatch', expected, expected => actual.match(expected instanceof RegExp ? expected : new RegExp(expected)) !== null),
+    };
+    return Object.freeze({
+        ...matchers,
+        get not() {
+            notFlag = true;
+            return matchers;
+        }
+    });
+}
