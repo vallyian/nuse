5b402c85312d93177572a2e47d43ba762041fda2
vallyian <vallyian@gmail.com>
2022-05-17 09:40:23 +0300

    update self

--- nuse.bat
+++ nuse.bat
@@ -1,11 +1,13 @@
 @echo off
 
-set "debugger=%2"
-if "%debugger%"=="true" @echo [34m[debug] debugger = '%debugger%' [0m
+set "nuseArg=%1"
+set "nuseDebug=%2"
+
+if "%nuseDebug%"=="true" @echo [34m[debug] nuseDebug = '%nuseDebug%' [0m
 
 ::env
 if "%nuseDir%"=="" (
-    if "%debugger%"=="true" @echo [34m[debug] nuseDir == '' [0m
+    if "%nuseDebug%"=="true" @echo [34m[debug] nuseDir = '%userprofile%\.nuse' [0m
     set "nuseDir=%userprofile%\.nuse"
     setx nuseDir "%userprofile%\.nuse"
     set "PATH=%userprofile%\.nuse;%path%"
@@ -15,44 +17,44 @@ set "nuseDirFile=%nuseDir%\v"
 
 ::install nuse
 if not exist "%nuseDir%" (
-    if "%debugger%"=="true" @echo [34m[debug] nuseDir '%nuseDir%' not exist [0m
+    if "%nuseDebug%"=="true" @echo [34m[debug] mkdir '%nuseDir%' [0m
     mkdir "%nuseDir%"
     set "nuseInitRegistry=1"
 )
 pushd "%~dp0"
 set "currentPath=%~dp0%~nx0"
-if "%debugger%"=="true" @echo [34m[debug] currentPath = '%currentPath%' [0m
+if "%nuseDebug%"=="true" @echo [34m[debug] currentPath = '%currentPath%' [0m
 if not exist "%nuseDir%\nuse.bat" (
-    if "%debugger%"=="true" @echo [34m[debug] nuse.bat '%nuseDir%\nuse.bat' not exist [0m
-    copy "%~dp0%~nx0" "%nuseDir%\nuse.bat" >nul
+    if "%nuseDebug%"=="true" @echo [34m[debug] copy '%currentPath%' '%nuseDir%\nuse.bat' [0m
+    copy "%currentPath%" "%nuseDir%\nuse.bat" >nul
     set "nuseInitRegistry=1"
 )
 popd
 if not exist "%nuseDir%\nuse.ps1" (
-    if "%debugger%"=="true" @echo [34m[debug] nuse.ps1 '%nuseDir%\nuse.ps1' not exist [0m
+    if "%nuseDebug%"=="true" @echo [34m[debug] create '%nuseDir%\nuse.ps1' [0m
     @echo nuse.bat @args > "%nuseDir%\nuse.ps1"
     set "nuseInitRegistry=1"
 )
 if not exist "%nuseDir%\node-latest.exe" (
     @echo getting latest node executable %nodeDistUrl%/latest/win-x64/node.exe ...
-    if "%debugger%"=="true" @echo [34m[debug] curl  '%nodeDistUrl%/latest/win-x64/node.exe' -o '%nuseDir%\node-latest.exe' [0m
+    if "%nuseDebug%"=="true" @echo [34m[debug] target '%nuseDir%\node-latest.exe' [0m
     curl "%nodeDistUrl%/latest/win-x64/node.exe" -o "%nuseDir%\node-latest.exe" >nul
     set "nuseInitRegistry=1"
 )
 if not exist "%nuseDir%\n-use.js" (
     @echo getting nuse runner https://raw.githubusercontent.com/vallyian/nuse/main/n-use.js ...
-    if "%debugger%"=="true" @echo [34m[debug] curl https://raw.githubusercontent.com/vallyian/nuse/main/n-use.js -o '%nuseDir%\n-use.js' [0m
+    if "%nuseDebug%"=="true" @echo [34m[debug] target '%nuseDir%\n-use.js' [0m
     curl https://raw.githubusercontent.com/vallyian/nuse/main/n-use.js -o "%nuseDir%\n-use.js" >nul
     set "nuseInitRegistry=1"
 )
 
 ::get wanted node version
-if "%debugger%"=="true" @echo [34m[debug] call '%nuseDir%\node-latest.exe' '%nuseDir%\n-use.js' '%1' [0m
-"%nuseDir%\node-latest.exe" "%nuseDir%\n-use.js" "%1" || exit /b 1
+if "%nuseDebug%"=="true" @echo [34m[debug] call '%nuseDir%\node-latest.exe' '%nuseDir%\n-use.js' '%nuseArg' [0m
+"%nuseDir%\node-latest.exe" "%nuseDir%\n-use.js" "%nuseArg%" || exit /b 1
 
 ::set new node path
 set /p nodeDir=<"%nuseDirFile%"
-if "%debugger%"=="true" @echo [34m[debug] nodeDir = '%nodeDir%' [0m
+if "%nuseDebug%"=="true" @echo [34m[debug] setx nodeDir '%nodeDir%' [0m
 setx nodeDir "%nodeDir%" >nul
 set "PATH=%nodeDir%;%path%"
 
@@ -61,16 +63,17 @@ for /F "tokens=*" %%x in ('node -v') do (set nodever=%%x)
 for /F "tokens=*" %%x in ('npm -v') do (set npmver=%%x)
 @echo [33mnode: %nodever%[0m
 @echo [33m npm: v%npmver%[0m
+@echo[
 
 ::cleanup
 if not "%currentPath%"=="%nuseDir%\nuse.bat" (
-if "%debugger%"=="true" @echo [34m[debug] currentPath '%currentPath%' != '%nuseDir%\nuse.bat' [0m
+if "%nuseDebug%"=="true" @echo [34m[debug] currentPath '%currentPath%' != '%nuseDir%\nuse.bat' [0m
 if not "%nuseDir%"=="." (
-    if "%debugger%"=="true" @echo [34m[debug] del '%currentPath%' [0m
-	del "%currentPath%"
-	@echo[
-	@echo setup completed successfully
-	@echo[
-	@echo (ignore the following "batch not found" message^)
+    if "%nuseDebug%"=="true" @echo [34m[debug] del '%nuseDir%\nuse.bat' [0m
+	del "%nuseDir%\nuse.bat"
+    if "%nuseDebug%"=="true" @echo [34m[debug] copy '%currentPath%' '%nuseDir%\nuse.bat' [0m
+    copy "%currentPath%" "%nuseDir%\nuse.bat" >nul
+    if "%nuseDebug%"=="true" @echo [34m[debug] del '%currentPath%' [0m
+	start cmd /c del "%currentPath%"
 )
 )
